a89174198f329c451a7ca636b730c9500b3e9273
uni51 <uni51@users.noreply.github.com>
2020-06-30 10:51:57 +0900

    9. 第9回 useEffect イベントリスナーを扱ってみよう

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import EffectHook from './component/EffectHook';
 // import ClassEffect from './component/ClassEffect';
+// import EffectHook from './component/EffectHook';
+import MouseEventEffect from './component/MouseEventEffect';
+
 
 function App() {
   return (
     <div className="App">
       {/* <ClassEffect /> */}
-      <EffectHook />
+      {/* <EffectHook /> */}
+      <MouseEventEffect />
     </div>
   );
 }
